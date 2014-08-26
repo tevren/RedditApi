8ba3ea7de0561a69bc7362bf4f7322c82c583691
Anurag Mohanty <amohanty@amplify.com>
2014-08-25 23:14:26 -0400

    single quotes install of double quotes

--- lib/RedditApi.rb
+++ lib/RedditApi.rb
@@ -1,8 +1,9 @@
-require "RedditApi/base"
-require "RedditApi/configuration"
-require "RedditApi/read"
-require "RedditApi/subscribe"
-require "RedditApi/version"
+require 'faraday'
+require 'RedditApi/base'
+require 'RedditApi/configuration'
+require 'RedditApi/read'
+require 'RedditApi/subscribe'
+require 'RedditApi/version'
 
 module RedditApi
   # Your code goes here...
